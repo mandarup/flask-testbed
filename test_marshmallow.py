5373c6bf32a364e2836a5d131f8536d4b3233d2b
Mandar Upadhye <mandarupadhye@gmail.com>
2018-07-22 21:29:52 -0400

    add marshmallow test

---
+++ test_marshmallow.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify
+from flask_marshmallow import Marshmallow
+
+
+app = Flask(__name__)
+ma = Marshmallow(app)
+
+from mongoengine import Document as Model
+from flask_marshmallow import Integer, String, DateTime
+
+class User(Model):
+    email = StringField()
+    password = StringField
+    date_created = Column(DateTime, auto_now_add=True)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('email', 'date_created', '_links')
+    # Smart hyperlinking
+    _links = ma.Hyperlinks({
+        'self': ma.URLFor('user_detail', id='<id>'),
+        'collection': ma.URLFor('users')
+    })
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+@app.route('/api/users/')
+def users():
+    all_users = User.all()
+    result = users_schema.dump(all_users)
+    return jsonify(result.data)
+    # OR
+    # return user_schema.jsonify(all_users)
+
+@app.route('/api/users/<id>')
+def user_detail(id):
+    user = User.get(id)
+    return user_schema.jsonify(user)
+
+if __name__ == "__main__":
+    app.run()
